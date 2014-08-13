9879db20a4e775ff8590fa8c6ec7168b632fe7be
johnnyleeothon <johnny.lee.othon@outlook.com>
2014-08-13 12:06:56 -0500

    Add error logging if rewriting a flag
    What if the user has no logging configured?

--- oriflamme/base.py
+++ oriflamme/base.py
@@ -1,3 +1,6 @@
+import logging
+
+logger = logging.getLogger(__name__)
 
 class FlagMixin(object):
 
@@ -6,5 +9,7 @@ class FlagMixin(object):
     def get_context_data(self, **kwargs):
         context = super(FlagMixin, self).get_context_data(**kwargs)
         for flag in self.template_flags:
+            if flag in context:
+                logger.error("Object %s already exists in %s" % (flag, self))
             context[flag] = True
         return context
